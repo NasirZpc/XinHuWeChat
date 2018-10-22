cc67b18523efac59e090710201072e45f23fdff2
NasirZpc <1041870188@qq.com>
2018-10-22 14:31:40 +0800

    模块2

--- pages/storeDetail/storeDetail.js
+++ pages/storeDetail/storeDetail.js
@@ -2,36 +2,34 @@ const app = getApp()
 
 Page({
     data:{
-        page:1,
-        storeLists:[]
+        shopid:'',
+        storeDetail:'',
     },
     onLoad(option) {
-        this.storeListsFunc()
+        this.setData({
+            shopid:option.shopid
+        })
+        this.storeDetailFunc()
     },
-    storeListsFunc(){
+    storeDetailFunc(){
         wx.showLoading({
             title: '玩命加载中',
         })
         //店铺列表
         wx.request({
-            url: app.baseUrl + 'index.php/Api/Shop/shoplist',
+            url: app.baseUrl + '',
             method: "POST",
             header: {
                 'content-type': 'application/x-www-form-urlencoded'
             },
             data: {
-                isindex: 0,
-                pagesize: 6,
-                p:this.data.page,
+                token:"",
+                shopid:this.data.shopid
             },
             success: (res) => {
                 if (res.data.status == 1) {
-                    var _storeLists = this.data.storeLists;
-                    for (var i = 0; i < res.data.data.length; i++) {
-                        _storeLists.push(res.data.data[i]);
-                    }
                     this.setData({
-                        storeLists: _storeLists
+                        storeDetail: res.data.data
                     })
                     // 隐藏加载框
                     setTimeout(()=>{
@@ -48,29 +46,10 @@ Page({
             }
         });
     },
-    /*上拉加载*/
-    onReachBottom() {
-        var that = this;
-        // 显示加载图标
-        wx.showLoading({
-            title: '玩命加载中',
-        })
-        // 页数+1
-        this.data.page = this.data.page + 1;
-        setTimeout(()=>{
-            this.storeListsFunc()
-        },500)
-    },
     /*商品详情页*/
     goProDetail(e){
         wx.navigateTo({
            url: "../proDetail/proDetail?proid="+e.currentTarget.dataset.id + '&isactivity=0'
-        });
-    },
-    /*店铺详情*/
-    goStoreDetail(e){
-        wx.navigateTo({
-           url: "../storeDetail/storeDetail?shopid="+e.currentTarget.dataset.id
         });
     },
 })
